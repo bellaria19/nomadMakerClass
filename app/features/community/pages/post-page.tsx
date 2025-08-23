6587ca55dd34cc7c615873e3abf3f48299bee4e8
bellaria19 <current11@naver.com>
2025-08-24 03:18:05 +0900

    Add Post Page

--- app/features/community/pages/post-page.tsx
+++ app/features/community/pages/post-page.tsx
@@ -1,4 +1,16 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "~/components/ui/breadcrumb";
 import type { Route } from "./+types/post-page";
+import { Form, Link } from "react-router";
+import { Button } from "~/components/ui/button";
+import { ChevronUpIcon, DotIcon } from "lucide-react";
+import { Textarea } from "~/components/ui/textarea";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 
 export const meta: Route.MetaFunction = ({ params }) => [
   { title: `Post ${params.postId} | wemake` },
@@ -10,8 +22,75 @@ export const meta: Route.MetaFunction = ({ params }) => [
 
 export default function PostPage({ params }: Route.ComponentProps) {
   return (
-    <div>
-      <h1>Post {params.postId}</h1>
+    <div className="grid grid-cols-6 gap-40 items-start">
+      <div className="col-span-4 space-y-10">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild>
+                <Link to="/community">Community</Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild>
+                <Link to="/community/?topic=productivity">Productivity</Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild>
+                <Link to={`/community/${params.postId}`}>
+                  What is the best productivity tool?
+                </Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+          </BreadcrumbList>
+        </Breadcrumb>
+        <div className="flex w-full items-start gap-10">
+          <Button variant={"outline"} className="flex flex-col h-14">
+            <ChevronUpIcon className="size-4 shrink-0" />
+            <span>10</span>
+          </Button>
+          <div className="space-y-20">
+            <div className="space-y-2">
+              <h2 className="text-3xl font-bold">
+                What is the best productivity tool?
+              </h2>
+              <div className="flex items-center gap-2.5 text-sm text-muted-foreground">
+                <span>@nico</span>
+                <DotIcon className="size-5" />
+                <span>12 hours ago</span>
+                <DotIcon className="size-5" />
+                <span>10 replies</span>
+              </div>
+              <p>
+                Hello, I'm looking for a productivity tool that can help me
+                manage my tasks and projects. Any recommendations? I have tried
+                Notion, but it's not what I'm looking for. I dream of a tool
+                that can help me manage my tasks and projects. Any
+                recommendations?
+              </p>
+            </div>
+            <Form className="flex items-start gap-5">
+              <Avatar>
+                <AvatarFallback>N</AvatarFallback>
+                <AvatarImage src="https://github.com/microsoft.png" />
+              </Avatar>
+              <div className="flex flex-col gap-5 w-full items-end">
+                <Textarea
+                  placeholder="Write a reply"
+                  className="w-full resize-none"
+                  rows={20}
+                />
+                <Button>Reply</Button>
+              </div>
+            </Form>
+          </div>
+        </div>
+      </div>
+      <aside className="col-span-2"></aside>
     </div>
   );
 }
--- app/features/community/pages/submit-post-page.tsx
+++ app/features/community/pages/submit-post-page.tsx
@@ -1,4 +1,9 @@
+import { Hero } from "~/components/hero-section";
 import type { Route } from "./+types/submit-post-page";
+import { Form } from "react-router";
+import InputPair from "~/components/input-pair";
+import SelectPair from "~/components/select-pair";
+import { Button } from "~/components/ui/button";
 
 export const meta: Route.MetaFunction = () => [
   { title: "Submit Post | wemake" },
@@ -10,8 +15,47 @@ export const meta: Route.MetaFunction = () => [
 
 export default function SubmitPostPage() {
   return (
-    <div>
-      <h1>Submit Post</h1>
+    <div className="space-y-20">
+      <Hero
+        title="Create Discussion"
+        description="Ask questions, share ideas, and connect with other developers"
+      />
+
+      <Form className="flex flex-col gap-10 max-w-screen-sm mx-auto">
+        <InputPair
+          label="Title"
+          name="title"
+          id="title"
+          description="(40 characters or less)"
+          required
+          placeholder="i.e What is the best productivity tool?"
+        />
+
+        <SelectPair
+          required
+          label="Category"
+          name="category"
+          description="Select the category that best fits your discussion"
+          placeholder="i.e Productivity"
+          options={[
+            { label: "Productivity", value: "productivity" },
+            { label: "Programming", value: "programming" },
+            { label: "Design", value: "design" },
+          ]}
+        />
+
+        <InputPair
+          label="Content"
+          name="content"
+          id="content"
+          description="(1000 characters or less)"
+          required
+          placeholder="i.e What is the best productivity tool?"
+          textarea
+        />
+
+        <Button className="">Create Discussion</Button>
+      </Form>
     </div>
   );
 }
